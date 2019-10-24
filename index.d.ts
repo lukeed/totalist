42b5b44171fc378293d58abcc43b66e7a5447eb4
Luke Edwards <luke.edwards05@gmail.com>
2019-10-23 20:27:04 -0700

    fix(types): export `Caller` type

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import { Stats } from 'fs';
 
-type Caller = (relPath: string, absPath: string, stats: Stats) => any;
+export type Caller = (relPath: string, absPath: string, stats: Stats) => any;
 declare function totalist(dir: string, callback: Caller): Promise<void>;
 export default totalist;
