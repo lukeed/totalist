61e4e673532756d82410e5628559307556d24201
Luke Edwards <luke.edwards05@gmail.com>
2020-06-20 16:51:23 -0700

    fix(types): add `prefix` argument;
    - technically exists, not encouraged

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 declare module 'totalist' {
 	import { Stats } from 'fs';
 	export type Caller = (relPath: string, absPath: string, stats: Stats) => any;
-	function totalist(dir: string, callback: Caller): Promise<void>;
+	function totalist(dir: string, callback: Caller, prefix?: string): Promise<void>;
 	export = totalist;
 }
 
 declare module 'totalist/sync' {
 	import { Stats } from 'fs';
 	export type Caller = (relPath: string, absPath: string, stats: Stats) => any;
-	function totalist(dir: string, callback: Caller): void;
+	function totalist(dir: string, callback: Caller, prefix?: string): void;
 	export = totalist;
 }
--- sync.d.ts
+++ sync.d.ts
@@ -1,4 +1,4 @@
 import { Stats } from 'fs';
 export type Caller = (relPath: string, absPath: string, stats: Stats) => any;
-declare function totalist(dir: string, callback: Caller): void;
+declare function totalist(dir: string, callback: Caller, prefix?: string): void;
 export default totalist;
