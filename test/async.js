07ec1c9638fc3c5191c70fc8447c353e8953cd58
Luke Edwards <luke.edwards05@gmail.com>
2020-06-20 16:39:18 -0700

    chore: update test runner

--- test/async.js
+++ test/async.js
@@ -1,18 +1,16 @@
-import test from 'tape';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 import { join, normalize } from 'path';
 import totalist from '../src/async';
 
 const fixtures = join(__dirname, 'fixtures');
 
-test('(async) exports', t => {
-	t.is(typeof totalist, 'function');
-	t.end();
+test('(async) exports', () => {
+	assert.type(totalist, 'function');
 });
 
 
-test('(async) usage :: relative', async t => {
-	t.plan(21);
-
+test('(async) usage :: relative', async () => {
 	let count = 0;
 	let styles = [];
 	let scripts = [];
@@ -20,22 +18,20 @@ test('(async) usage :: relative', async t => {
 	// +3 x 6
 	await totalist('test/fixtures', (name, abs, stats) => {
 		count++;
-		t.false(stats.isDirectory(), 'file is not a directory');
-		t.true(abs.startsWith(fixtures), '~> `abs` is absolute');
-		t.false(name.startsWith(fixtures), '~> `name` is NOT absolute');
+		assert.not(stats.isDirectory(), 'file is not a directory');
+		assert.ok(abs.startsWith(fixtures), '~> `abs` is absolute');
+		assert.not(name.startsWith(fixtures), '~> `name` is NOT absolute');
 		(/\.js$/.test(name) ? scripts : styles).push(name);
 	});
 
-	t.is(count, 6, 'saw 6 files total');
+	assert.is(count, 6, 'saw 6 files total');
 
-	t.same(scripts.sort(), ['aaa/index.js', 'bbb/ccc/index.js', 'bbb/index.js'].map(normalize).sort(), 'script `name` values are expected');
-	t.same(styles.sort(), ['aaa/index.css', 'bbb/ccc/index.css', 'bbb/index.css'].map(normalize).sort(), 'style `name` values are expected');
+	assert.equal(scripts.sort(), ['aaa/index.js', 'bbb/ccc/index.js', 'bbb/index.js'].map(normalize).sort(), 'script `name` values are expected');
+	assert.equal(styles.sort(), ['aaa/index.css', 'bbb/ccc/index.css', 'bbb/index.css'].map(normalize).sort(), 'style `name` values are expected');
 });
 
 
-test('(async) usage :: absolute', async t => {
-	t.plan(21);
-
+test('(async) usage :: absolute', async () => {
 	let count = 0;
 	let styles = [];
 	let scripts = [];
@@ -43,14 +39,17 @@ test('(async) usage :: absolute', async t => {
 	// +3 x 6
 	await totalist(fixtures, (name, abs, stats) => {
 		count++;
-		t.false(stats.isDirectory(), 'file is not a directory');
-		t.true(abs.startsWith(fixtures), '~> `abs` is absolute');
-		t.false(name.startsWith(fixtures), '~> `name` is NOT absolute');
+		assert.not(stats.isDirectory(), 'file is not a directory');
+		assert.ok(abs.startsWith(fixtures), '~> `abs` is absolute');
+		assert.not(name.startsWith(fixtures), '~> `name` is NOT absolute');
 		(/\.js$/.test(name) ? scripts : styles).push(name);
 	});
 
-	t.is(count, 6, 'saw 6 files total');
+	assert.is(count, 6, 'saw 6 files total');
 
-	t.same(scripts.sort(), ['aaa/index.js', 'bbb/ccc/index.js', 'bbb/index.js'].map(normalize).sort(), 'script `name` values are expected');
-	t.same(styles.sort(), ['aaa/index.css', 'bbb/ccc/index.css', 'bbb/index.css'].map(normalize).sort(), 'style `name` values are expected');
+	assert.equal(scripts.sort(), ['aaa/index.js', 'bbb/ccc/index.js', 'bbb/index.js'].map(normalize).sort(), 'script `name` values are expected');
+	assert.equal(styles.sort(), ['aaa/index.css', 'bbb/ccc/index.css', 'bbb/index.css'].map(normalize).sort(), 'style `name` values are expected');
 });
+
+
+test.run();
--- test/sync.js
+++ test/sync.js
@@ -1,18 +1,15 @@
-import test from 'tape';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 import { join, normalize } from 'path';
 import totalist from '../src/sync';
 
 const fixtures = join(__dirname, 'fixtures');
 
-test('(sync) exports', t => {
-	t.is(typeof totalist, 'function');
-	t.end();
+test('exports', () => {
+	assert.type(totalist, 'function');
 });
 
-
-test('(sync) usage :: relative', t => {
-	t.plan(21);
-
+test('usage :: relative', () => {
 	let count = 0;
 	let styles = [];
 	let scripts = [];
@@ -20,22 +17,20 @@ test('(sync) usage :: relative', t => {
 	// +3 x 6
 	totalist('test/fixtures', (name, abs, stats) => {
 		count++;
-		t.false(stats.isDirectory(), 'file is not a directory');
-		t.true(abs.startsWith(fixtures), '~> `abs` is absolute');
-		t.false(name.startsWith(fixtures), '~> `name` is NOT absolute');
+		assert.not(stats.isDirectory(), 'file is not a directory');
+		assert.ok(abs.startsWith(fixtures), '~> `abs` is absolute');
+		assert.not(name.startsWith(fixtures), '~> `name` is NOT absolute');
 		(/\.js$/.test(name) ? scripts : styles).push(name);
 	});
 
-	t.is(count, 6, 'saw 6 files total');
+	assert.is(count, 6, 'saw 6 files total');
 
-	t.same(scripts, ['aaa/index.js', 'bbb/ccc/index.js', 'bbb/index.js'].map(normalize), 'script `name` values are expected');
-	t.same(styles, ['aaa/index.css', 'bbb/ccc/index.css', 'bbb/index.css'].map(normalize), 'style `name` values are expected');
+	assert.equal(scripts, ['aaa/index.js', 'bbb/ccc/index.js', 'bbb/index.js'].map(normalize), 'script `name` values are expected');
+	assert.equal(styles, ['aaa/index.css', 'bbb/ccc/index.css', 'bbb/index.css'].map(normalize), 'style `name` values are expected');
 });
 
 
-test('(sync) usage :: absolute', t => {
-	t.plan(21);
-
+test('usage :: absolute', () => {
 	let count = 0;
 	let styles = [];
 	let scripts = [];
@@ -43,14 +38,17 @@ test('(sync) usage :: absolute', t => {
 	// +3 x 6
 	totalist(fixtures, (name, abs, stats) => {
 		count++;
-		t.false(stats.isDirectory(), 'file is not a directory');
-		t.true(abs.startsWith(fixtures), '~> `abs` is absolute');
-		t.false(name.startsWith(fixtures), '~> `name` is NOT absolute');
+		assert.not(stats.isDirectory(), 'file is not a directory');
+		assert.ok(abs.startsWith(fixtures), '~> `abs` is absolute');
+		assert.not(name.startsWith(fixtures), '~> `name` is NOT absolute');
 		(/\.js$/.test(name) ? scripts : styles).push(name);
 	});
 
-	t.is(count, 6, 'saw 6 files total');
+	assert.is(count, 6, 'saw 6 files total');
 
-	t.same(scripts, ['aaa/index.js', 'bbb/ccc/index.js', 'bbb/index.js'].map(normalize), 'script `name` values are expected');
-	t.same(styles, ['aaa/index.css', 'bbb/ccc/index.css', 'bbb/index.css'].map(normalize), 'style `name` values are expected');
+	assert.equal(scripts, ['aaa/index.js', 'bbb/ccc/index.js', 'bbb/index.js'].map(normalize), 'script `name` values are expected');
+	assert.equal(styles, ['aaa/index.css', 'bbb/ccc/index.css', 'bbb/index.css'].map(normalize), 'style `name` values are expected');
 });
+
+
+test.run();
