5f86a6e3f173d89d46ee24eaaf408d516426169c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-10-21 08:41:15 -0400

    fix(sync): avoid symlink loops
    Check the chain of directories that led to the current directory
    before reading its contents. Avoid reading the same directory twice
    when crawling deeper.
    
    The `realpathSync` result is never passed to the callback, so all
    file paths are still inside the root directory.

--- src/sync.js
+++ src/sync.js
@@ -1,15 +1,17 @@
 import { join, resolve } from 'path';
-import { readdirSync, statSync } from 'fs';
+import { readdirSync, statSync, realpathSync } from 'fs';
 
-export function totalist(dir, callback, pre='') {
+export function totalist(dir, callback, pre='', chain=[]) {
 	dir = resolve('.', dir);
-	let arr = readdirSync(dir);
+	let link = realpathSync.native(dir);
+	if (chain.includes(link)) return;
+	let arr = readdirSync(link);
 	let i=0, abs, stats;
 	for (; i < arr.length; i++) {
 		abs = join(dir, arr[i]);
 		stats = statSync(abs);
 		stats.isDirectory()
-			? totalist(abs, callback, join(pre, arr[i]))
+			? totalist(abs, callback, join(pre, arr[i]), chain.concat(link))
 			: callback(join(pre, arr[i]), abs, stats);
 	}
 }
